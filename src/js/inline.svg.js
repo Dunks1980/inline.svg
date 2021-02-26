fd168d3cd9fdf7c0b92720cd260f9dec7b37945a
Ian <dunks1980@gmail.com>
2021-02-26 17:42:55 +0000

    better logging

--- src/js/inline.svg.js
+++ src/js/inline.svg.js
@@ -1,16 +1,28 @@
 const inlinesvg = (query, callback) => {
-  let arrOfEls;
+  let arrOfEls,
+    els = document.querySelectorAll(query),
+    count = 0,
+    red = 'color:red;',
+    yellow = 'color:yellow;',
+    green = 'color: lightgreen;',
+    inherit = 'color:inherit;',
+    code = 'font-size: 16px; font-family: "Courier New", monospace;',
+    prefix = 'inline.svg: ',
+    c = '%c';
   const endsWith = (str, suffix) => {
     return str.slice(-suffix.length) === suffix;
   };
   const getImage = (el, href, count, last_image) => {
     let hasCallback = callback && typeof callback === 'function';
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", href);
+    xhr.open('GET', href);
     xhr.onload = () => {
       el.insertAdjacentHTML('afterend', xhr.responseText.trim());
       if (hasCallback) {
-        arrOfEls.push({url: href, element: el.nextSibling});
+        arrOfEls.push({
+          url: href,
+          element: el.nextSibling
+        });
       }
       el.parentNode.removeChild(el);
       if (last_image && hasCallback) {
@@ -19,33 +31,42 @@ const inlinesvg = (query, callback) => {
     };
     xhr.send();
   };
-  let els = document.querySelectorAll(query);
-  let count = 0;
   if (els && els.length) {
-    //console.log('els-' + els.length);
     arrOfEls = [];
     [].forEach.call(els, (el) => {
-      if (!el) return console.log("%cinlineSvgFile: %cElement not found ", 'color:red', 'color:inherit');
-      if (el.tagName !== 'A') {
-        console.log('-------------------------------------------------------------------------------------------');
-        console.log("%cinlineSvgFile: %cElement needs to be a Anchor Tag, Example:", 'color:red', 'color:inherit');
-        console.log(`%c${el.outerHTML}`, "color:red; font-size: 16px; font-family: 'Courier New', monospace;");
-        console.log("Change to:");
-        console.log(`%c<a${el.classList.length ? ' class="'+el.classList+'"' : ''}${el.id ? ' id="'+el.id+'"' : ''}${el.src ? ' href="path_to_file.svg"' : ''}></a>`, "color: lightgreen; font-size: 16px; font-family: 'Courier New', monospace;");
-        console.log('-------------------------------------------------------------------------------------------');
-      } else {
+      if (!el) {
+        return console.log(c + prefix + c + 'Element not found', red, inherit);
+      }
+      if (el.tagName.toLowerCase() === 'a') {
         let href = el.href;
-        if (!href) return console.log("%cinlineSvgFile: %cNo href found", 'color:red', 'color:inherit');
+        if (!href) return console.log(c + prefix + c + 'No href found', red, inherit);
         if (endsWith(href, '.svg')) {
           count++;
           getImage(el, href, (count).toString(), Number(count) === Number(els.length));
         } else {
-          return console.log("%cinlineSvgFile: %cCan only convert svg files", 'color:red', 'color:inherit');
+          return console.log(c + prefix + c + 'Can only convert svg files', red, inherit);
         }
+      } else {
+        console.log(c + '\n\n' + prefix + c + 'Element needs to be a Anchor Tag, Example:\n', red, inherit);
+        console.log(c + `\t${el.outerHTML}\n`, red + code);
+        console.log('Change to:\n\n');
+        console.log(c + `\t<a ${
+          el.classList ?
+            `class="${el.classList}" ` : ``
+          }${
+          el.id ?
+            `id="${el.id}" ` : ``
+          }href="/my-file.svg"></a>\n\n`,
+          green + code
+        );
       }
     });
   } else {
-    return console.log("%cinlineSvgFile: %cNo elements found for the selector %c" + query, 'color:red', 'color:inherit', 'color:yellow');
+    return console.log(c + prefix + c + 'No elements found for the selector ' + c + query,
+      red,
+      inherit,
+      yellow
+    );
   }
 };
 if (typeof exports != "undefined") {
