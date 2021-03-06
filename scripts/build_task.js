251e07c06d3fc7c45e24f040a1a60c6d7d3d112a
Ian <dunks1980@gmail.com>
2021-03-06 11:32:03 +0000

    build update

--- scripts/build_task.js
+++ scripts/build_task.js
@@ -1,24 +1,24 @@
-// const fs = require('fs');
+const fs = require('fs');
 const execSync = require('child_process').execSync;
 
-// function deleteFolderRecursive(path) {
-//   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-//     fs.readdirSync(path).forEach(function(file, index){
-//       var curPath = path + "/" + file;
-//       if (fs.lstatSync(curPath).isDirectory()) { // recurse
-//         deleteFolderRecursive(curPath);
-//       } else { // delete file
-//         fs.unlinkSync(curPath);
-//       }
-//     });
-//     fs.rmdirSync(path);
-//   }
-// }
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
 
-// deleteFolderRecursive("./dist");
+deleteFolderRecursive("./dist");
 
 // run the parcel scripts
-let parcel_scripts = 'parcel build src/index.html';
+let parcel_scripts = 'parcel build src/index.html --no-content-hash --no-source-maps';
 
 console.log(execSync(parcel_scripts, {encoding: 'utf8'}));
 
